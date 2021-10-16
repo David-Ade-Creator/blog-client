8c46f9e875603e354f3cae68414c058ac9db5cc4
David-Ade-Creator <www.davidadeyemi@gmail.com>
2021-10-16 07:57:19 +0530

    created responsive navbar

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -3,6 +3,12 @@ import Link from 'next/link';
 
 function Header({className}) {
     const [scrollPosition, setScrollPosition] = React.useState(0);
+    const [navbarVisible, setNavbarVisible] = React.useState(false);
+
+    const toggleNavbar = () =>{
+        setNavbarVisible(!navbarVisible)
+    }
+
     const handleScroll = () => {
         const position = window.pageYOffset;
         setScrollPosition(position);
@@ -20,7 +26,14 @@ function Header({className}) {
         <header className={`header ${scrollPosition >= "80" ? "scroll-header" : ""}`}>
             <div className="nav_container container">
               <div className="logo"><Link href="/"><a>The Halftimers</a></Link></div>
-              <button className="button_secondary header_button"><i class="uil uil-align-justify"></i></button>
+              <ul className={`nav_links_container ${navbarVisible ? "show_nav_links" : ""}`}>
+                  <li><Link href="/transfer"><a className="nav_link">Transfer</a></Link></li>
+                  <li ><Link href="/championsleague"><a className="nav_link">Champions-League</a></Link></li>
+                  <li ><Link href="/league"><a className="nav_link">League</a></Link></li>
+                  <li ><Link href="/international"><a className="nav_link">International</a></Link></li>
+                  <li ><Link href="/videos"><a className="nav_link">Vidoes</a></Link></li>
+              </ul>
+              <span className="button_secondary header_button" onClick={toggleNavbar}><i class="uil uil-align-justify"></i></span>
             </div>
         </header>
     )
